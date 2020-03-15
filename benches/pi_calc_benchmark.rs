f048abef475652012079d64297094319a1d3977b
Nikolay Danailov <frostblooded@yahoo.com>
2020-03-15 10:59:50 +0200

    Refactor code and fix calculations for multithreaded tests

--- benches/pi_calc_benchmark.rs
+++ benches/pi_calc_benchmark.rs
@@ -5,18 +5,22 @@ use bigdecimal::*;
 use std::thread;
 use std::sync::*;
 
-fn factorial(n: u32) -> BigDecimal {
-    let mut result = bigdecimal::One::one();
+fn new_num(n: u64) -> BigDecimal {
+    BigDecimal::from(n)
+}
+
+fn factorial(n: u64) -> BigDecimal {
+    let mut result = new_num(1);
 
     for i in 2..=n {
-        result *= BigDecimal::from(i);
+        result *= new_num(i);
     }
 
     result
 }
 
-fn pow(b: &BigDecimal, power: u32) -> BigDecimal {
-    let mut res = bigdecimal::One::one();
+fn pow(b: &BigDecimal, power: u64) -> BigDecimal {
+    let mut res = new_num(1);
 
     for _ in 1..=power {
         res *= b;
@@ -25,19 +29,18 @@ fn pow(b: &BigDecimal, power: u32) -> BigDecimal {
     res
 }
 
-fn calc_series_no_threads_no_cache(n: u32) -> BigDecimal {
-    let mut pi = bigdecimal::Zero::zero();
-
-    let a = BigDecimal::from(1103);
-    let b = BigDecimal::from(26390);
-    let c = BigDecimal::from(396);
+fn calc_series_no_threads_no_cache(n: u64) -> BigDecimal {
+    let mut pi = new_num(0);
+    let a = new_num(1103);
+    let b = new_num(26390);
+    let c = new_num(396);
 
     for k in 0..=n {
-        pi += (factorial(4 * k) * (&a + &b * BigDecimal::from(k))) /
+        pi += (factorial(4 * k) * (&a + &b * new_num(k))) /
               (pow(&factorial(k), 4) * pow(&c, 4 * k));
     }
     
-    pi *= (BigDecimal::from(2) * BigDecimal::from(2).sqrt().unwrap()) / BigDecimal::from(9801);
+    pi *= (new_num(2) * new_num(2).sqrt().unwrap()) / new_num(9801);
     pi = 1 / pi;
     pi
 }
@@ -50,20 +53,20 @@ impl FactorialCalculator {
     fn new() -> Self {
         FactorialCalculator {
             cache: Mutex::new(vec![
-                BigDecimal::from(1),
-                BigDecimal::from(1)
+                new_num(1),
+                new_num(1)
             ])
         }
     }
 
-    fn is_calculated(&self, n: u32) -> bool {
+    fn is_calculated(&self, n: u64) -> bool {
         self.cache.lock().unwrap().len() >= ((n + 1) as usize)
     }
 
-    fn calc(&self, n: u32) -> BigDecimal {
+    fn calc(&self, n: u64) -> BigDecimal {
         if !self.is_calculated(n) {
             let prev = self.calc(n - 1);
-            self.cache.lock().unwrap().push(prev * BigDecimal::from(n));
+            self.cache.lock().unwrap().push(prev * new_num(n));
         }
         
         let cache_borrow = self.cache.lock().unwrap();
@@ -71,48 +74,44 @@ impl FactorialCalculator {
     }
 }
 
-fn calc_series_no_threads_with_cache(n: u32) -> BigDecimal {
-    let mut pi = bigdecimal::Zero::zero();
-
+fn calc_series_no_threads_with_cache(n: u64) -> BigDecimal {
+    let mut pi = new_num(0);
+    let a = new_num(1103);
+    let b = new_num(26390);
+    let c = new_num(396);
     let factorial_calculator = FactorialCalculator::new();
-    let a = BigDecimal::from(1103);
-    let b = BigDecimal::from(26390);
-    let c = BigDecimal::from(396);
 
     for k in 0..=n {
-        pi += (factorial_calculator.calc(4 * k) * (&a + &b * BigDecimal::from(k))) /
+        pi += (factorial_calculator.calc(4 * k) * (&a + &b * new_num(k))) /
               (pow(&factorial_calculator.calc(k), 4) * pow(&c, 4 * k));
     }
     
-    pi *= (BigDecimal::from(2) * BigDecimal::from(2).sqrt().unwrap()) / BigDecimal::from(9801);
+    pi *= (new_num(2) * new_num(2).sqrt().unwrap()) / new_num(9801);
     pi = 1 / pi;
     pi
 }
 
-fn calc_series_for_range(start_index: u32, end_index: u32) -> BigDecimal {
-    let mut pi = bigdecimal::Zero::zero();
-
-    let a = BigDecimal::from(1103);
-    let b = BigDecimal::from(26390);
-    let c = BigDecimal::from(396);
+fn calc_series_for_range(start_index: u64, end_index: u64) -> BigDecimal {
+    let mut pi = new_num(0);
+    let a = new_num(1103);
+    let b = new_num(26390);
+    let c = new_num(396);
 
     for k in start_index..=end_index {
-        pi += (factorial(4 * k) * (&a + &b * BigDecimal::from(k))) /
+        pi += (factorial(4 * k) * (&a + &b * new_num(k))) /
               (pow(&factorial(k), 4) * pow(&c, 4 * k));
     }
     
-    pi *= (BigDecimal::from(2) * BigDecimal::from(2).sqrt().unwrap()) / BigDecimal::from(9801);
-    pi = 1 / pi;
-    pi
+    pi * (new_num(2) * new_num(2).sqrt().unwrap()) / new_num(9801)
 }
 
-fn calc_series_with_threads_no_cache(n: u32) -> BigDecimal {
+fn calc_series_with_threads_no_cache(n: u64) -> BigDecimal {
     if n < 4 {
         return calc_series_no_threads_no_cache(n);
     }
 
     let mut handles = vec![];
-    const THREAD_COUNT: u32 = 4u32;
+    const THREAD_COUNT: u64 = 4u64;
     let jobs_per_thread = n / THREAD_COUNT;
     let remaining_jobs = n % THREAD_COUNT;
 
@@ -135,34 +134,32 @@ fn calc_series_with_threads_no_cache(n: u32) -> BigDecimal {
         result += handle.join().expect("Thread finished with error");
     }
 
+    result = 1 / result;
     result
 }
 
 
-fn calc_series_for_range_with_cache(start_index: u32, end_index: u32, factorial_calculator: Arc<FactorialCalculator>) -> BigDecimal {
-    let mut pi = bigdecimal::Zero::zero();
-
-    let a = BigDecimal::from(1103);
-    let b = BigDecimal::from(26390);
-    let c = BigDecimal::from(396);
+fn calc_series_for_range_with_cache(start_index: u64, end_index: u64, factorial_calculator: Arc<FactorialCalculator>) -> BigDecimal {
+    let mut pi = new_num(0);
+    let a = new_num(1103);
+    let b = new_num(26390);
+    let c = new_num(396);
 
     for k in start_index..=end_index {
-        pi += (factorial_calculator.calc(4 * k) * (&a + &b * BigDecimal::from(k))) /
+        pi += (factorial_calculator.calc(4 * k) * (&a + &b * new_num(k))) /
               (pow(&factorial_calculator.calc(k), 4) * pow(&c, 4 * k));
     }
     
-    pi *= (BigDecimal::from(2) * BigDecimal::from(2).sqrt().unwrap()) / BigDecimal::from(9801);
-    pi = 1 / pi;
-    pi
+    pi * (new_num(2) * new_num(2).sqrt().unwrap()) / new_num(9801)
 }
 
-fn calc_series_with_threads_with_cache(n: u32) -> BigDecimal {
+fn calc_series_with_threads_with_cache(n: u64) -> BigDecimal {
     if n < 4 {
         return calc_series_no_threads_no_cache(n);
     }
 
     let mut handles = vec![];
-    const THREAD_COUNT: u32 = 4u32;
+    const THREAD_COUNT: u64 = 4u64;
     let jobs_per_thread = n / THREAD_COUNT;
     let remaining_jobs = n % THREAD_COUNT;
     let factorial_calculator = Arc::new(FactorialCalculator::new());
@@ -189,13 +186,16 @@ fn calc_series_with_threads_with_cache(n: u32) -> BigDecimal {
         result += handle.join().expect("Thread finished with error");
     }
 
+    result = 1 / result;
     result
 }
 
 fn calc_series_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("calc series");
 
-    for &i in [1u32, 10u32, 50u32, 100u32].iter() {
+    let keypoints: Vec<u64> = vec![5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+    for i in keypoints {
         group.bench_function(BenchmarkId::new("no threads no cache", i), |b| b.iter(|| calc_series_no_threads_no_cache(i)));
         group.bench_function(BenchmarkId::new("no threads with cache", i), |b| b.iter(|| calc_series_no_threads_with_cache(i)));
         group.bench_function(BenchmarkId::new("with threads no cache", i), |b| b.iter(|| calc_series_with_threads_no_cache(i)));
