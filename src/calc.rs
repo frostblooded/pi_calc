55a15f35f548645bf1cce93e441d170d05a8a8db
Nikolay Danailov <frostblooded@yahoo.com>
2020-05-02 09:35:15 +0300

    Fix task creation. It was creating the tasks incorrectly at the end

--- src/calc.rs
+++ src/calc.rs
@@ -91,16 +91,19 @@ fn calc_series_helper_with_threads(
 ) -> BigNum {
     let total_start_time = SystemTime::now();
     let mut handles = vec![];
-    let tasks_count = 50;
-    let iters_per_task = n / tasks_count;
-    let remaining_iters = n % tasks_count;
+    let wanted_tasks_count = 10;
+    let iters_per_task = ((n as f64) / (wanted_tasks_count as f64)).ceil() as u64;
+    let remaining_iters = n % wanted_tasks_count;
+    let tasks_count = n / iters_per_task + 1;
 
-    let iter_range: Vec<_> = (0..(tasks_count - 1)).collect();
+    // We are excluding the iters for the last task, because they
+    // are added additionally as remainder.
+    let iter_range: Vec<_> = (0..(tasks_count - 2)).collect();
 
-    let mut start_indexes: Vec<_> = iter_range.iter().map(|i| i * iters_per_task).collect();
+    let mut start_indexes: Vec<u64> = iter_range.iter().map(|i| i * iters_per_task).collect();
     start_indexes.push(n - iters_per_task - remaining_iters);
 
-    let mut end_indexes: Vec<_> = iter_range
+    let mut end_indexes: Vec<u64> = iter_range
         .iter()
         .map(|i| (i + 1) * iters_per_task - 1)
         .collect();
