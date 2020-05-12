d7cf3a0ca26cd5457872ea56fa88142a2dffa72b
Nikolay Danailov <frostblooded@yahoo.com>
2020-05-12 11:50:09 +0300

    Prereserve the capacity for the factorial cache

--- src/factorial_calculator.rs
+++ src/factorial_calculator.rs
@@ -6,7 +6,7 @@ pub struct FactorialCalculator {
 
 impl FactorialCalculator {
     pub fn new(precision: u32, n: u64) -> Self {
-        let mut cache_builder: Vec<BigNum> = vec![];
+        let mut cache_builder: Vec<BigNum> = Vec::with_capacity(n as usize);
         cache_builder.push(new_num(precision, 1));
 
         for i in 1..=n {
